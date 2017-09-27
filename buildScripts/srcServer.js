43ac9c7feb4a54474e21ac8882ac4d651cb8e26d
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2017-09-27 11:19:39 +0100

    Add comment to the srcServer.js file

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,6 +4,7 @@ var open = require("open");
 
 var port = 3000;
 var app = express();
+//Here we are requesting the root and sending the file specify in ../src/index.html
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../src/index.html"));
 });
@@ -12,6 +13,7 @@ app.listen(port, function(err) {
   if (err) {
     console.log(err);
   } else {
+    // Here we are opening the application on this address on this port
     open("http://localhost:" + port);
   }
 });
